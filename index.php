fae7590b52f1112ae4ba9b35bf262a4af8c555e4
NoctumEU <mlkr@protonmail.ch>
2017-05-21 23:39:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,104 @@
+<html class="fa-events-icons-ready">
+
+<title>Noctum.eu</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<link rel="icon" type="image/png" href="http://vignette3.wikia.nocookie.net/whitewolf/images/f/f6/DemonTheFallenSymbol.png/revision/latest?cb=20150903145513">
+
+<link href="//use.fontawesome.com/d87a9922b6.css" media="all" rel="stylesheet">
+
+</head>
+
+<body onload="document.body.style.opacity='1'" oncontextmenu="return false" style="opacity: 1;">
+   
+   
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-78257851-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
+   
+   <script src="//use.fontawesome.com/d87a9922b6.js"></script>
+   <script src="title.js" tppabs="./title.js"></script>
+   
+   <link href="./css" rel="stylesheet" type="text/css">
+    
+	<link rel="stylesheet" href="./style.css">
+
+    <section class="first">
+
+        <div class="wrap">
+          
+		  <div class="ferhatr">Noctum.eu</div>
+            
+			<marquee class="html-marquee" direction="right" behavior="scroll" scrollamount="4.5">
+             
+			 <p class="fade active"></a></p>
+            
+			</marquee>
+           
+		   <center> <a class="cta outline fade active" href="http://steamcommunity.com/id/qhucs"><span>Steam</span></a> <a class="cta outline fade active" href="http://t.me/Huxleyy"><span>Telegram</span></a> <a class="cta outline fade active" href="mailto:lol@noctum.eu"><span>Email</span></a>
+			</center>
+        
+		</div>
+
+<div class="container">
+            <div class="killFeed">
+                <a class="killer">Noctum.eu</a>
+                <div class="weapon scar"></div>
+                <div class="wallbang true"></div>
+                <div class="headshot true"></div>
+                <a class="killed">Gibonacci.xyz</a>
+            </div>
+			<div style="clear:both;">
+			<div class="killFeed">
+                <a class="killer">Noctum.eu</a>
+                <div class="weapon ak47"></div>
+                <div class="wallbang true"></div>
+                <div class="headshot true"></div>
+                <a class="killed">Huxley</a>
+            </div>
+			<div style="clear:both;">
+			<div class="killFeed">
+                <a class="killer">Noctum.eu</a>
+                <div class="weapon ak47"></div>
+                <div class="wallbang true"></div>
+                <div class="headshot true"></div>
+                <a class="killed">yxne.us</a>
+</a>
+            </div>
+			<div style="clear:both;">
+			<div class="killFeed">
+                <a class="killer">Noctum.eu</a>
+                <div class="weapon scar"></div>
+                <div class="wallbang true"></div>
+                <div class="headshot true"></div>
+                <a class="killed">BitEmpire</a>
+            </div>
+			<div style="clear:both;">
+    </section>
+
+
+    </div>
+    </div>
+    </section>
+
+<center>Your IP Address has been logged: <? echo $_SERVER["REMOTE_ADDR"]; ?>
+
+
+    <video src="robin.mp4" id="bgvid" type="video/mp4" style="" autoplay="" loop=""></video>
+    <script>
+        var video = document.getElementById('bgvid');
+        video.volume = 0.35;
+    </script>
+
+
+
+</body>
+
+</html>
---
+++ title.js
@@ -0,0 +1,66 @@
+if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+
+function loaded(){
+	
+	setInterval(loop, 600);
+
+}
+
+<!-- CODE FROM sklz.io -->
+var x = 0;
+
+var titleText = [ "N O C T U M", "O C T U M", "C T U M", "T U M", "U M","M", "U M", "T U M", "C T U M", "O C T U M", "N O C T U M"];
+
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
+<!-- CODE FROM sklz.io -->
+
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+function load(){
+	var videoAmount = 3;
+	var video = document.getElementsByTagName("video")[0];
+	video.src = "video"+Math.ceil(Math.random()*videoAmount)+".mp4";
+	video.volume = 0.3;
+	video.load();
+	setInterval(loop, 1000);
+	}
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 64)
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 65
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
